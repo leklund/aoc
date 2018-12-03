c526bb180ab4eec13d04caeda4faa02610e217cd
Lukas Eklund <leklund@fastly.com>
2018-12-03 12:09:49 -0600

    day 1 and day 2

---
+++ 2018/02_1.rb
@@ -0,0 +1,287 @@
+def run(input)
+  counters = {
+    2 => 0,
+    3 => 0
+  }
+
+  input.split.each do |id|
+    letter_counts = id.split('').group_by { |l| l }.map { |_k, v| v.size }
+    counters.each do |k, _v|
+      counters[k] += 1 if letter_counts.include?(k)
+    end
+  end
+  counters[2] * counters[3]
+end
+
+def run_tests
+  input = <<-INPUT
+    abcdef
+    bababc
+    abbcde
+    abcccd
+    aabcdd
+    abcdee
+    ababab
+  INPUT
+  expected = 12
+  actual = run(input)
+  abort "failure: expected #{actual} to eq #{expected}" unless actual == expected
+end
+
+data = DATA.read
+
+run_tests
+puts run(data)
+
+
+__END__
+lsrivfotzgdxpkefaqmuiygchj
+lsrivfotzqdxpkeraqmewygchj
+lsrivfotzbdepkenarjuwygchj
+lsrivfotwbdxpkeoaqmunygchj
+lsrijfotzbdxpkenwqmuyygchj
+lsrivfotzbdxpkensqsuwygcdj
+lsrivfotubdxpkenzqmuwyschj
+lsrjvwotzbdxpkenaqjuwygchj
+lsrtvfotzbdxpkeaaqmuqygchj
+lscivzotzbdxpkenaqmuwygcnj
+ddrivfotzbdxpkenlqmuwygchj
+jsrivfvtzbdxpkenaqmufygchj
+csrivfotzxdxpkenaqguwygchj
+lprivfbtzbqxpkenaqmuwygchj
+lsrnvfotzbnxpkenaqmuwygchk
+lsiivfotzbdhpkencqmuwygchj
+lsrivfotzbyxpkenaqmzwygchc
+lsjivfotqbdxpvenaqmuwygchj
+lsrivfotzbdxpkencqmuwvgqhj
+lsrivfotzhdxpqenaqouwygchj
+lsrivfytzbnxpkenaqmuwygcsj
+llrivfotzbdxpkenaqmuwynchd
+lsuivfotzbdxpnenaqmuwygchk
+lsrtvootnbdxpkenaqmuwygchj
+ysrivfotzcdxpkenaqmuwhgchj
+lsrivfotxbdxpkefgqmuwygchj
+lsrmvfotzbaxpkenaqmuwygfhj
+lsrivfothbyxpkxnaqmuwygchj
+isrivfotzbdxpkenaqmkwygcht
+lhrivfotzbdxpkbnfqmuwygchj
+lsrivfotzbmxpkenaqmuwbgdhj
+lsrivvotzbdxcoenaqmuwygchj
+ssrwvfotzbdjpkenaqmuwygchj
+lsrivfotgbwxpkenaqmhwygchj
+lsrivfotzbdxpkenaqcuhygcyj
+lcdivfotzbdxpkenaqmuwxgchj
+ysbivfotzbdxpkenaqmuwkgchj
+lsrivfltzbdxxkenaqcuwygchj
+lsrivfotzbdxpkgnaqmunegchj
+fsqpvfotzbdxpkenaqmuwygchj
+lsriifotzbcxpkenaqmubygchj
+lsrivfotzjdxpkenaqmugygcjj
+tsrikfotzbdxpkeneqmuwygchj
+larivfotzbdxpkenaqmwwygcpj
+larivfotzbdxpkenaqmuayrchj
+lsravfotzbdxpkdoaqmuwygchj
+lsrivfotzbixpkenaqvtwygchj
+lsrixfotnbdxtkenaqmuwygchj
+lsrirfotzbdxpkeneqmuwygchv
+lsrivfofzedxpkenaqmswygchj
+lwrivfotzvdxpkenaqmuwygfhj
+lsrivfotzbdapkenaqmuqygehj
+lsrizfotgbdxpkenaqjuwygchj
+lsrivioxzbdxpkanaqmuwygchj
+lsrivfmtzbdxpkgnaqmuwcgchj
+lsrivfotzbdxpkeaaqmuofgchj
+lsrivfotvbdxpkenuqmuwygcht
+lsrivfothcdxpkenaqouwygchj
+lsgivfotzbdxpkenawmuwygchi
+lsrigfotzbdxpmonaqmuwygchj
+lsrivfotzbdxrkfnaqmuwygcha
+lsrivfopobdxpkenaqmuwygchv
+lsrejfotzbdxpkvnaqmuwygchj
+lsrivfotzbdxplenqqmuwygchz
+lsmivfotzbdppkebaqmuwygchj
+lsrivfotubdipkewaqmuwygchj
+lsrivfodnbhxpkenaqmuwygchj
+lsrivfotzbdxpkenaqmkwzgshj
+lsrixfotzbdxpkensqmuwygohj
+lsdivfotzbdxpkenaqmuwagcwj
+lsrimfotzbdxpkenaqmuwygcyu
+asnivfotzbdxpkenaqmbwygchj
+lseivfltfbdxpkenaqmuwygchj
+lsrivfbtzbdxpuenaqmuwyychj
+lsziafozzbdxpkenaqmuwygchj
+lsrivfotzbdxpkwnaomuwygchg
+ldrivfotzbdxpkeniqmuwygihj
+lsrivfotzbdxpkenaqhdwycchj
+lsrigfotzbdxphenaqmuwynchj
+lsripfotzbdxpgenaqmuwygchh
+lsrgvfoczbdxpkenaqmuwygihj
+lsribfotzbgxpkenaqhuwygchj
+lsrkvfotztdxpienaqmuwygchj
+lsrivfohzbdxpkenaqrxwygchj
+lsrivfoszbdxpkenavmuwygvhj
+lsrivfstzblxpkcnaqmuwygchj
+lzrivfotzbdxpkegaqmuwygchv
+lsrivtotzbdxpkenaqrumygchj
+lsgivfotzbdwpkenaqmuwhgchj
+lurivfotybdxpkenaqmuwygchg
+lsrivfogzbdxpkmnrqmuwygchj
+lsrivgotzbdxpkengqmuwygcwj
+lirivfbyzbdxpkenaqmuwygchj
+lwrivfotzbdxpkbjaqmuwygchj
+lsrivkotzbqxakenaqmuwygchj
+lxrivfotzbdxpkenaqmuwygshy
+lxxivfqtzbdxpkenaqmuwygchj
+lsrivfohzbdxpzenaqmuzygchj
+lsrivfotzndxekenaqmuwygcvj
+lsrdvfotzbdxpkenaqguiygchj
+lsrivfotzbdxpiehaqauwygchj
+atrivfotzbdxpkenaqmuwygchz
+lsrivfovzbdxpkenaqmuvygcwj
+lsrivfotzmdxpkennqmuwyxchj
+luvcvfotzbdxpkenaqmuwygchj
+lsriqfotzbdxpbenaqmuwygchg
+bsoivfotzudxpkenaqmuwygchj
+lsrivfotzbdxphenaqmhwxgchj
+lsrivfotzbdxpkenasmuwjgchw
+lsrivdotzboxpkenaqmuwykchj
+lsqivfotzbdxcdenaqmuwygchj
+lsrivfktzndypkenaqmuwygchj
+lwrivfotzbdxpkenaqmuolgchj
+lkrivfowzbdxpkenaqmbwygchj
+lsrivhotzbdxpkenaqyuwygvhj
+lsruvfotzbdxpkecaqmukygchj
+lsrivdotzbdsskenaqmuwygchj
+lsrivfotzbdxpkanaqmuwygcmc
+lsrgvfotubdxpkenrqmuwygchj
+psrivfotzbdxpkenaqmutygchd
+lsrivfitzbdxpkenagmiwygchj
+lsrivfotzbdxpkbnaqauwyrchj
+lsrivfotvbdxpjenaqmuwygchr
+lsrdvfoyzbjxpkenaqmuwygchj
+vsrivfothbdxpkenaqmuwyychj
+lyrivfotzpdxpkepaqmuwygchj
+lsgbqfotzbdxpkenaqmuwygchj
+lxrivfotzbdxpkenegmuwygchj
+lsrivfokzbdxpkenaqnuwyxchj
+lsrivfotubmxpkexaqmuwygchj
+lswivfvtzbdxpkenaqmuwygcgj
+lsrivfonzbdxpkenaqiuwygchc
+isrivlotzbdxpkenaqmuwygchf
+lsrilfozzbdxpkenaqmuwygcvj
+wsrivfotzbdxpkepaqmuwegchj
+lsrivfrtzbrxpkenaqquwygchj
+lsrivfotzbdxpkeqaqmuoygjhj
+lsrivfotzmdxpkenaqmuwyxchg
+lsrnvfotzbzxxkenaqmuwygchj
+ldrivfotzbdxpkenaqmlxygchj
+lsriofotzbdxpkenaqmwwmgchj
+lsrivfotzodxjkenaqmuwyglhj
+lsriviotzbdxpkegaqguwygchj
+lsrimfotzbdxpkanaqmuwygshj
+lwrzvfotzbdxpkenaqmuwygcfj
+lirivfotzbdxkkenvqmuwygchj
+lsrivfotlbdxpkeoaqmuwygahj
+lsxivfotzbdxpkenaqmuwwgchi
+lsrivfotzbdxpkenaqmukygzzj
+lsrtvfotzbdxskenaqmuwygcij
+lsgilfotzbdxpdenaqmuwygchj
+lsriyfotbbdxpkenaqmuwygchm
+lsrivfotabdxpkenaqmuwyghhs
+xsrizfotzbdxpkenaqmuwygczj
+lsrivfotybdxpkenaqquwygchx
+lsrzvfofzbdxpktnaqmuwygchj
+xsripfotzbdxpkenaqmqwygchj
+lsrivfotzbdspkenahmuwugchj
+lsmivfotzbdbpkenaqmuwygchy
+lsruvfotzbdxpkenaqqpwygchj
+lrmivfotzbdxpkenaqguwygchj
+lsnivfotzbdlpketaqmuwygchj
+lsrivfotzbdxjketaqjuwygchj
+lsrivxotzbdchkenaqmuwygchj
+lsrivootzbdxpkenaqmuwybmhj
+tsrivfdtzbdxpkenaqmuwpgchj
+lsrivmotzbdxpkxnaqmuwcgchj
+lsrivfotzadepkenaqmuwyichj
+dsrivfotrbdxpkenaqmuwtgchj
+lsrivfhtzbdxvkenoqmuwygchj
+lsrivfotzvdxbkenaqmbwygchj
+lsrxvcotzbdxpkenaqmuwygvhj
+lsrivfotzbdxykenaqmuwfgcha
+lsbivfotzbdxpkenaqmuwfvchj
+lfrivfotzbdcpkgnaqmuwygchj
+lsrivfotzbdxpwegdqmuwygchj
+lsrivfotyjdupkenaqmuwygchj
+gsrivfotzbdxpkenaemuwcgchj
+lsrivfodqbdxpkenaqmuwygchg
+lsrivfoczbdxpkenaqnuwwgchj
+lsrivpouzbhxpkenaqmuwygchj
+llbivuotzbdxpkenaqmuwygchj
+lfrivfofzbdxpkenaqmuwygchb
+lsrivfotzbdxpkenaumuwgghhj
+lsrivfotzbdxqaenazmuwygchj
+lsrivfotzbgxpkenkqmqwygchj
+lsrivfotzbdxpkensqiawygchj
+ljrijfotzbdxppenaqmuwygchj
+lsrivfoszbdxpkrnlqmuwygchj
+lsrijfotzbdxpcfnaqmuwygchj
+lsrivfotzbdopkebaqmuwytchj
+lsrivfonzbdxnkenalmuwygchj
+larivfouzbvxpkenaqmuwygchj
+lsryvfotzbdxpkensqmuwygyhj
+lsrivfztzbdxpkenaxmuwigchj
+lqkivfotzbdxpkenaqmuwygcht
+wsdivfotzbdxpkenbqmuwygchj
+lsrlvfotzadxpkencqmuwygchj
+lsrivfotoohxpkenaqmuwygchj
+lsrivfbuzbdfpkenaqmuwygchj
+psrivfotzbdxpkenawmuqygchj
+lsrivmotzbdxpkxnaqmuwcychj
+lsrivfotzvdgpkenaqmuwlgchj
+lcfivfstzbdxpkenaqmuwygchj
+lsrivfotzbddpkeeaqmuwygcij
+lsribfotzbdxpkenaqmuwugcyj
+lsrivfotzbdxakenaqmkwygyhj
+lsrivfotzbdxpkegaqmupyvchj
+lfrivfitzbdxpkenaqmuwygcrj
+lskivfotzbdxpkenaqmuwygwwj
+lsrivfotzddnpkenaqmuwfgchj
+lsrivfotzbdiukhnaqmuwygchj
+lfrivfotzbdxpkendqmuwygctj
+ljriqfotzvdxpkenaqmuwygchj
+lsrivfotzbdxpkeskqpuwygchj
+lsrivfotzbdxpkehaqmupygghj
+lsriyfotsbdxpkedaqmuwygchj
+lsrivfotzbdsjsenaqmuwygchj
+lsrivfotzbwxpienaqmuaygchj
+lsrivrotzbdxpkenaumuwygahj
+lsrivpotzfdxpkenaqmuwyjchj
+lsrivfomebdxpoenaqmuwygchj
+lswigfotpbdxpkenaqmuwygchj
+lsrivnotzbdxpkenaqmufrgchj
+lsrivfolbbdxpkenaqmuwygcqj
+lirivfotzbdxpknnaqeuwygchj
+lsrrvfxtzbdxpaenaqmuwygchj
+lspivfotzbdxpnsnaqmuwygchj
+lsrivfotzbyxpkenaqmawygcij
+lsrivfotzbfxpbenaqmuwyichj
+lsrivfotzbvxpjeyaqmuwygchj
+lyrihfotzbdxpknnaqmuwygchj
+uurivfotzbdxpkenaqmubygchj
+lsrivfotgbdxnkenaxmuwygchj
+lsriffotzbdxpkdnaqmuwygshj
+lsrisfotzbdxpkenaqzjwygchj
+lsrilfotzbdxpkenaqmuwygtgj
+lsrivfotzbdxzkenaqmuhmgchj
+hsrivfotzbdxprenaqauwygchj
+tsrevfotzbdupkenaqmuwygchj
+lsrizfotzbpxpkenaqmuwyrchj
+lsdivfotzbxxpkenaqmuhygchj
+lsrivfttzbyxpkenaqmuaygchj
+lsrivfotzodxpwenaqzuwygchj
+lsrivfotfbdxpkenaqvuwygyhj
+lsrivfotzzdxpknnaqmulygchj
+lsrjvvotzbdxpkenaqmuwjgchj
+lsrivuotzbdxpkeiaqxuwygchj
+lsrivfotzbdxpzenaqmmwygthj
+lsrivfotzbdxphenaqmuwyghvj
