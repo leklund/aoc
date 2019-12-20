9fadca83af36449060e58c40daac254e8c800bae
Lukas Eklund <leklund@fastly.com>
2019-12-20 10:24:13 -0600

    day 9 prep: port computer to new file and use int64

--- 2019/day_07/amp.go
+++ 2019/day_07/amp.go
@@ -1,55 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 )
 
-// HLT Halt opcode
-const HLT = 99
-
-// ADD - addition opcode
-const ADD = 1
-
-// MUL - multiplication opcode
-const MUL = 2
-
-// INP - input opcode
-const INP = 3
-
-// OUT - output opcode
-const OUT = 4
-
-// JIT - Jump if true opcode
-const JIT = 5
-
-// JIF - Jump if false opcode
-const JIF = 6
-
-// LT - less than opcode
-const LT = 7
-
-// EQL 0 equality opcode
-const EQL = 8
-
-var pointerIncr = map[int]int{
-	ADD: 4,
-	MUL: 4,
-	INP: 2,
-	OUT: 2,
-	HLT: 0,
-	JIT: 3,
-	JIF: 3,
-	LT:  4,
-	EQL: 4,
-}
-
 func main() {
-	line := getLine("input.txt")
-	prog := splitAndToI(line)
+	prog := makeProg("input.txt")
 
 	signal := amp(prog)
 
@@ -61,25 +17,24 @@ func main() {
 	fmt.Println(signal2)
 }
 
-func amp(program []int) int {
+func amp(program Program) int64 {
 	p0 := []int{0, 1, 2, 3, 4}
 	permutations := [][]int{}
-	max := 0
-	in := make(chan int)
-	out := make(chan int)
+	max := int64(0)
+	in := make(chan int64)
+	out := make(chan int64)
 
 	generatePermutations(len(p0), p0, &permutations)
 	fmt.Println("phases size:", len(permutations))
 
 	for _, phases := range permutations {
 
-		output := 0
+		output := int64(0)
 		for _, phase := range phases {
-			p := make([]int, len(program))
-			copy(p, program)
+			p := program.dup()
 
-			go run(p, in, out)
-			in <- phase
+			go p.run(in, out)
+			in <- int64(phase)
 			in <- output
 			output = <-out
 		}
@@ -91,32 +46,31 @@ func amp(program []int) int {
 	return max
 }
 
-func ampFeedback(program []int) int {
+func ampFeedback(program Program) int64 {
 	p0 := []int{5, 6, 7, 8, 9}
 	permutations := [][]int{}
-	max := 0
+	max := int64(0)
 
 	// one input and output channel per amplifier
 	// could likely be simplified to be one slice of chans
-	in := make([]chan int, 5)
-	out := make([]chan int, 5)
+	in := make([]chan int64, 5)
+	out := make([]chan int64, 5)
 
 	generatePermutations(len(p0), p0, &permutations)
 
 	for _, phases := range permutations {
 		for i, phase := range phases {
-			p := make([]int, len(program))
-			copy(p, program)
+			p := program.dup()
 
-			in[i] = make(chan int)
-			out[i] = make(chan int)
+			in[i] = make(chan int64)
+			out[i] = make(chan int64)
 
-			go run(program, in[i], out[i])
+			go p.run(in[i], out[i])
 
-			in[i] <- phase
+			in[i] <- int64(phase)
 		}
 
-		output := 0
+		output := int64(0)
 		running := true
 
 		for running {
@@ -140,79 +94,6 @@ func ampFeedback(program []int) int {
 	return max
 }
 
-func run(program []int, in, out chan int) {
-
-	var pointer int
-
-	for program[pointer] != HLT {
-		var params [3]int
-		var valuePointer int
-
-		opcode, modes := parseOpcode(program[pointer])
-
-		incr := pointerIncr[opcode]
-
-		for i := 1; i < incr; i++ {
-			if modes[i-1] == 0 {
-				valuePointer = program[pointer+i]
-			} else {
-				valuePointer = pointer + i
-			}
-
-			params[i-1] = program[valuePointer]
-
-		}
-		switch opcode {
-		case HLT:
-			break
-		case ADD:
-			program[valuePointer] = params[0] + params[1]
-		case MUL:
-			program[valuePointer] = params[0] * params[1]
-		case INP:
-			program[valuePointer] = <-in
-		case OUT:
-			out <- program[valuePointer]
-		case JIT:
-			if params[0] > 0 {
-				pointer = params[1]
-				continue
-			}
-		case JIF:
-			if params[0] == 0 {
-				pointer = params[1]
-				continue
-			}
-		case LT:
-			v := 0
-			if params[0] < params[1] {
-				v = 1
-			}
-			program[valuePointer] = v
-		case EQL:
-			v := 0
-			if params[0] == params[1] {
-				v = 1
-			}
-			program[valuePointer] = v
-		}
-
-		pointer += incr
-	}
-}
-
-func parseOpcode(opcode int) (int, [3]int) {
-	var modes [3]int
-	op := opcode % 100
-	opcode /= 100
-
-	for i := 0; i < 3; i++ {
-		modes[i] = opcode % 10
-		opcode /= 10
-	}
-	return op, modes
-}
-
 // Heap's algorithim (https://en.wikipedia.org/wiki/Heap%27s_algorithm)
 func generatePermutations(k int, a []int, res *[][]int) {
 	if k == 1 {
@@ -230,40 +111,4 @@ func generatePermutations(k int, a []int, res *[][]int) {
 		}
 
 	}
-}
-
-func getLine(path string) string {
-	file, err := os.Open(path)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer file.Close()
-
-	s := bufio.NewScanner(file)
-	s.Split(bufio.ScanLines)
-
-	s.Scan()
-	return s.Text()
-}
-
-func splitAndToI(line string) []int {
-	s := strings.Split(line, ",")
-	var p []int
-	for _, inst := range s {
-		p = append(p, toI(inst))
-	}
-
-	return p
-}
-
-func toI(s string) int {
-	i, err := strconv.Atoi(s)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return i
 }
